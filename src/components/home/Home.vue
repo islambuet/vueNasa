81c8fdb561707c6efc86f72ef978184d7ad625ab
Md. Shaiful Islam <islambuet@gmail.com>
2020-11-24 09:34:26 +0600

    APOD completed

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,7 +2,26 @@
   <div>
       <div v-if="$system_variables.status_task_loaded==1">
         <a-card>
-          This is HomePage
+          <a-date-picker @change="onDateChange" />
+          <a-button type="primary" @click="getAPOD">show data</a-button>
+        </a-card>
+        <a-card>
+          <div v-if="loaded">
+            <table>
+              <tr>
+                <td>Title</td>
+                <td>{{data.title}}</td>
+              </tr>
+              <tr>
+                <td>Description</td>
+                <td>{{data.explanation}}</td>
+              </tr>
+            </table>      
+            <img style="max-width:100%;" alt="Vue logo" :src="data.hdurl">      
+          </div>
+          <div v-else>
+            {{message}}
+          </div>
         </a-card>
         
       </div>
@@ -22,14 +41,46 @@ export default {
            
   },
   data() {
-    return {
-      value: '',
+    return {      
+      date:'',
+      loaded:false,
+      message:'Loading api please wait',
+      data:{}
       
     }
   },
-  methods: {    
-    
-    
+  methods: {
+    onDateChange(date, dateString) {
+      this.date=dateString;      
+    },
+    getAPOD:function()
+    {
+      this.loaded=false;
+      if(this.date==""){
+        this.message="Please Select a Date";
+      }
+      else{
+        
+        this.$axios.get('https://api.nasa.gov/planetary/apod?api_key='+this.$system_variables.apiKey+'&date='+this.date)
+        .then((response) => {
+          if(response.data)
+          {
+            this.data=response.data;
+            this.loaded=true;
+          }
+          else
+          {
+            this.message=response.msg;
+          }
+          
+          
+        })
+        .catch(error => {  
+              this.message="Api Response Error";
+              
+        }); 
+      }
+    }
     
   },
 }
